8e0f4cb8b28067136a1e89e8349802dca3a43645
jopamo <p@1g4.org>
2022-11-10 14:34:51 -0600

    keep the footer on bottom

--- script.js
+++ script.js
@@ -142,3 +142,8 @@ function zipFormatter() {
 
 $(zipFormatter);
 
+$(document).ready(function () {
+    var docHeight = $('#content').height();
+    var wantedHeight = docHeight - 376;
+    $('#content').css({ "min-height": wantedHeight });
+});
